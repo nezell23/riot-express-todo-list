3d56ff71800b0e3562f407586c9f5900a5c404f1
Nichole Ezell <nezell8487@gmail.com>
2021-08-07 17:39:14 -0400

    updated IP address

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
     ssh
         .connect({
             // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-            host: '100.26.244.58',
+            host: '54.172.191.2',
             username: 'ubuntu',
             privateKey: 'todo-key.pem'
         })
